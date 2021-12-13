97ff53100b82c66e04451e644dcfd08a480fc7d4
YatharthVyas <yatharth2000.vyas@gmail.com>
2021-12-13 20:07:01 +0000

    Deploying to gh-pages from @ YatharthVyas/developer-portfolio@44b11a71f42b55bb2648318b0c9a54b022d872a6 🚀

---
+++ _next/static/e7xe3YUip3P231oOU5ZF8/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-40647496e977b500ac66.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
